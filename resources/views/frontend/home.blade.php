a030420be83b69b21d96629d1b16417069bef626
sarker majid <majidursarkar333@gmail.com>
2025-04-27 14:07:00 +0600

    'update'

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -2,11 +2,70 @@
 
 @section('frontend')
     <!-- ======= Hero Section ======= -->
-    <img src="{{ asset('frontend/assets/img/slide/hero-img.jpeg') }}" style="margin-top: 120px;" alt="" width="100%">
+    {{-- <img src="{{ asset('frontend/assets/img/slide/hero-img.jpeg') }}" style="margin-top: 120px;" alt="" width="100%"> --}}
     <!-- End Hero -->
+    <!-- ======= Hero Section ======= -->
+    <section id="hero">
+        <div id="heroCarousel" data-bs-interval="2000" class="carousel slide carousel-fade" data-bs-ride="carousel">
+
+            <div class="carousel-inner" role="listbox">
+
+                <!-- Slide 1 -->
+                <div class="carousel-item active" style="background-image: url('{{ asset('frontend/assets/img/slide/slider-1.webp') }}');">
+                    <div class="carousel-container">
+                        {{-- <div class="carousel-content animate__animated animate__fadeInUp">
+                            <h2>Welcome to <span>Company</span></h2>
+                            <p>Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut
+                                aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque
+                                accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
+                            <div class="text-center"><a href="" class="btn-get-started">Read More</a></div>
+                        </div> --}}
+                    </div>
+                </div>
+
+                <!-- Slide 2 -->
+                <div class="carousel-item" style="background-image: url('{{ asset('frontend/assets/img/slide/slider-2.webp') }}');">
+                    <div class="carousel-container">
+                        {{-- <div class="carousel-content animate__animated animate__fadeInUp">
+                            <h2>Lorem Ipsum Dolor</h2>
+                            <p>Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut
+                                aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque
+                                accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
+                            <div class="text-center"><a href="" class="btn-get-started">Read More</a></div>
+                        </div> --}}
+                    </div>
+                </div>
+
+                <!-- Slide 3 -->
+                <div class="carousel-item" style="background-image: url('{{ asset('frontend/assets/img/slide/slider-3.webp') }}');">
+                    <div class="carousel-container">
+                        {{-- <div class="carousel-content animate__animated animate__fadeInUp">
+                            <h2>Sequi ea ut et est quaerat</h2>
+                            <p>Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut
+                                aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque
+                                accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
+                            <div class="text-center"><a href="" class="btn-get-started">Read More</a></div>
+                        </div> --}}
+                    </div>
+                </div>
+
+            </div>
+
+            <a class="carousel-control-prev" href="#heroCarousel" role="button" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon bi bi-chevron-left" aria-hidden="true"></span>
+            </a>
+
+            <a class="carousel-control-next" href="#heroCarousel" role="button" data-bs-slide="next">
+                <span class="carousel-control-next-icon bi bi-chevron-right" aria-hidden="true"></span>
+            </a>
+
+            <ol class="carousel-indicators" id="hero-carousel-indicators"></ol>
+
+        </div>
+    </section><!-- End Hero -->
+
 
     <main id="main">
-
         <!-- ======= About Us Section ======= -->
         <section id="about-us" class="about-us" style="margin-top: 30px;">
             <div class="container" data-aos="fade-up">
